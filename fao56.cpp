8a2bc4c9e2e3dd9793524f3bf5b354729f03224d
joaquincasanova <joaquin.casanova@gmail.com>
2015-04-24 16:26:45 -0400

    data

--- fao56.cpp
+++ fao56.cpp
@@ -6,5 +6,47 @@
 
 using namespace std;
 
+FILE *fid;
+string filename = "./FAWN_report.csv";
+
+type struct{
+	float Zu;
+	float uZ;
+	float u2;
+	int growth;
+	float Rn;
+RHmax
+RHmin
+RHmean
+Tair
+Tmax
+Tmin
+Tmean
+eA
+eTmax
+eTmin
+TT
+D
+H
+M
+Y
+delta
+P
+DT
+}
+Rnl
+Rns
+
+int readData(){
+	fid = fopen(filname,r);
+	while(fscanf()){}
+}
+
+int dailyVals(){
+}
+
+int vape(){
+}
+
 void main(){
 }
